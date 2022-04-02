059a29e193fba8253fcc2b019d29fd5044405981
nubidebs <debora.crescenzo@truepill.com>
2022-04-02 21:09:42 +0100

    connected disk and cpu modules to power module. Connected computer module to disk and cpu modules

--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService], // Accessible only inside the module
+  exports: [PowerService], // This is to make the module accessible by other modules
 })
 export class PowerModule {}
